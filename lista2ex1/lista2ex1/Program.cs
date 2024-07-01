015b57a128e36af4a7c49ac94144413c87f672a3
majopri <166362054+majopri@users.noreply.github.com>
2024-07-01 20:27:28 -0300

    Add files via upload

---
+++ lista2ex1/lista2ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("digite o primeiro valor:");
+            int valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o segundo valor:");
+            int valor2 = int.Parse(Console.ReadLine());
+           
+            Console.WriteLine("O maior valor e: {maior}");
+
+          
+
+            
+        }
+    }
+}
